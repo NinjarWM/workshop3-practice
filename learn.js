f38d4bab0bc5c574ed9e3bb63634666bd4bdc2a5
unknown <wijarinee.m@gmail.com>
2026-02-18 15:00:12 +0700

    It's a happy day!

--- learn.js
+++ learn.js
@@ -3,3 +3,10 @@ console.log("Hello from JavaScript!");
 let score = 0;               // A number
 let playerName = "Somchai";  // A string  (text)
 let isPlaying = true;        // A boolean (true/false)
+// Print them out
+console.log("Player:", playerName);
+console.log("Score:", score);
+console.log("Playing:", isPlaying);
+// Change a variable
+score = 10;
+console.log("New score:", score);
