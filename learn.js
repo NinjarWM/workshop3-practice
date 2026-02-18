c92fb76b7cc47f364a73e4b0ff2ec7b41d4f4af1
unknown <wijarinee.m@gmail.com>
2026-02-18 14:52:11 +0700

    Hi there!

---
+++ learn.js
@@ -0,0 +1,5 @@
+console.log("Hello from JavaScript!");
+//Variable - storing different types of information
+let score = 0;               // A number
+let playerName = "Somchai";  // A string  (text)
+let isPlaying = true;        // A boolean (true/false)
